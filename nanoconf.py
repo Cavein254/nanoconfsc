eccdef0f4a17d816484a2bc0a1f5a3eeb7f8a4eb
johnmuthua <caveomuthama@gmail.com>
2025-07-14 11:00:50 +0300

    feat: add .gitignore file

---
+++ nanoconf.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+import os
+import yaml
+from box import Box
+import contextlib
+import json
+
+class NanoConf:
+    def __init__(self, cfg_path=None):
+        self._cfg_path = cfg_path
+        self._name = self._cfg_path.name
+        if self._cfg_path.is_dir():
+            for sub in self._cfg_path.glob("*.nconf"):
+                new_conf = NanoConf(sub)
+                setattr(self, new_conf._name, new_conf)
+        elif self._cfg_path.is_file() and self._cfg_path.suffix == ".nconf":
+            self._load_config()
+            self._pull_envar()
+
+    def _load_config(self):
+        cfg_dict = yaml.load(self._cfg_path.read_text(), Loader=yaml.SafeLoader)
+        for sub_import in cfg_dict.pop("_import",[]):
+            new_conf = NanoConf(
+                self._cfg_path / sub_import
+                if self._cfg_path.is_dir()
+                else self._cfg_path.parent / sub_import
+            )
+            setattr(self, new_conf._name, new_conf)
+        self.__dict__.update(Box(cfg_dict))
+    
+    def _pull_envar(self):
+        if getattr(self, "_envar_prefix",""):
+            for key, val in os.environ.items():
+                if key.startswith(self._envar_prefix):
+                    if key.startswith(self._envar_prefix):
+                        key_name = key.replace(f"{self._envar_prefix}_", "")
+                        with contextlib.supress(json.decoder.JSONDecodeError):
+                            val = Box(json.loads(val))
+                        self.__dict__[key_name] = val
+    
+    def __repr__(self):
+        return (
+            f"<NanoConf: {self._name}.("
+            + ", ".join(filter(lambda x: not x.startswith("_"), self.__dict__.keys()))
+            + ")>"
+            )
